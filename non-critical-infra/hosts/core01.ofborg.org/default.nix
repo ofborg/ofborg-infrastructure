b8db936cf2f953f11bbd797614dbb04771c8566f
Janne Heß <janne@hess.ooo>
2024-12-29 17:42:05 +0100

    Generalize out a common module

--- non-critical-infra/hosts/core01.ofborg.org/default.nix
+++ non-critical-infra/hosts/core01.ofborg.org/default.nix
@@ -2,11 +2,8 @@
 
 {
   imports = [
-    inputs.srvos.nixosModules.server
     inputs.srvos.nixosModules.hardware-hetzner-cloud
-    "${inputs.infra}/modules/common.nix"
-    "${inputs.infra}/non-critical-infra/modules/common.nix"
-    "${inputs.infra}/non-critical-infra/modules/prometheus/node-exporter.nix"
+    ../../modules/ofborg/common.nix
     ./nginx.nix
     ./rabbitmq.nix
     ./ofborg-config.nix
@@ -20,15 +17,6 @@
     ./github-comment-poster.nix
   ];
   # TODO backups
-  # TODO wire up exporters
-  # TODO loki
-
-  # Not part of the infra team
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM35Bq87SBWrEcoDqrZFOXyAmV/PJrSSu3hl3TdVvo4C janne"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMcTaqUZSwv6YW8lx/JhsAZTdNSSC2fR8Pgk8woeFKh cole-h"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3+NUShVVqdTH93fYFIVr0uaZ2zGiU9UEWIFk1sDHID cole-h-2"
-  ];
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -55,15 +43,6 @@
       }
     ];
     linkConfig.RequiredForOnline = "routable";
-  };
-
-  nixpkgs.overlays = [ (_self: super: {
-    ofborg = inputs.ofborg.packages.${super.system}.pkg;
-  }) ];
-
-  systemd.targets.ofborg = {
-    description = "ofBorg target";
-    wantedBy = [ "multi-user.target" ];
   };
 
   system.stateVersion = "24.11"; # Did you read the comment?
---
+++ non-critical-infra/modules/ofborg/common.nix
@@ -0,0 +1,30 @@
+{ inputs, ... }:
+
+{
+  imports = [
+    inputs.srvos.nixosModules.server
+    "${inputs.infra}/modules/common.nix"
+    "${inputs.infra}/non-critical-infra/modules/common.nix"
+    "${inputs.infra}/non-critical-infra/modules/prometheus/node-exporter.nix"
+  ];
+
+  # TODO wire up exporters
+  # TODO loki
+
+  # Not part of the infra team
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM35Bq87SBWrEcoDqrZFOXyAmV/PJrSSu3hl3TdVvo4C janne"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMcTaqUZSwv6YW8lx/JhsAZTdNSSC2fR8Pgk8woeFKh cole-h"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3+NUShVVqdTH93fYFIVr0uaZ2zGiU9UEWIFk1sDHID cole-h-2"
+  ];
+
+  nixpkgs.overlays = [ (_self: super: {
+    ofborg = inputs.ofborg.packages.${super.system}.pkg;
+  }) ];
+
+  systemd.targets.ofborg = {
+    description = "ofBorg target";
+    wantedBy = [ "multi-user.target" ];
+  };
+
+}
