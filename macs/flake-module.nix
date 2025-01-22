d34bcfb42e8dc2c855ac27140b9ceca540328b12
Janne Heß <janne@hess.ooo>
2025-01-22 22:45:29 +0100

    Add the last x86 mac

--- macs/flake-module.nix
+++ macs/flake-module.nix
@@ -36,12 +36,16 @@
           # 32 GB RAM
           # 500 GB disk
         }
+        {
+          hostname = "nixos-foundation-macstadium-44911507";
+          system = "x86_64";
+          ip = "208.83.1.186";
+          # 12 CPU cores
+          # 32 GB RAM
+          # 500 GB disk
+        }
       ]
   );
-  # nixos-foundation-macstadium-44911305 root@208.83.1.173, x86_64,  12 cores, 32GB RAM, 500GB disk
-  # nixos-foundation-macstadium-44911362 root@208.83.1.175, x86_64,  12 cores, 32GB RAM, 500GB disk
-  # nixos-foundation-macstadium-44911507 root@208.83.1.186, x86_64,  12 cores, 32GB RAM, 500GB disk
-
   # nixos-foundation-macstadium-44911207 root@208.83.1.145, aarch64,  8 cores, 16GB RAM, 256GB disk
   # nixos-foundation-macstadium-44911104 root@208.83.1.181, aarch64,  8 cores, 16GB RAM, 256GB disk
 }
